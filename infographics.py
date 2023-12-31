6f4408cac2d906a4265143ac2cf483fb5d617650
ASHIKAMOHAN <145342597+ASHIKAMOHAN@users.noreply.github.com>
2023-12-31 01:07:54 +0000

    subplots added

--- infographics.py
+++ infographics.py
@@ -1,98 +1,3 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import seaborn as sns
-
-# # Original data URI
-# Original_dataUri = 'Life-Expectancy-Data-Updated.csv'
-
-# # List of countries to filter
-# countryList = ["Belgium", "India", "United Arab Emirates", "Bangladesh", "Brazil", "South Africa", "Kuwait"]
-
-# # Load the data
-# data = pd.read_csv(Original_dataUri)
-
-# # Filter data by countries in countryList
-# filtered_data = data[data["Country"].isin(countryList)]
-# # Get rid of any empty data
-# filtered_data.dropna(inplace=True)
-# # Save filtered data to CSV
-# # filtered_data.to_csv("filtered.csv", index=False)
-
-# # Set the overall figure size
-# plt.figure(figsize=(12, 18))
-
-# # Add title space
-# plt.subplot(6, 1, 1)  # 6 rows, 1 column, position 1
-# plt.text(0.5, 0.5, 'Title of the Combined Image', horizontalalignment='center', verticalalignment='center', fontsize=16, fontweight='bold')
-# plt.axis('off')
-
-# # Distribution of infant deaths
-# plt.figure(figsize=(8, 6))
-# sns.histplot(filtered_data['Infant_deaths'], bins=20, kde=True)
-# plt.title('Distribution of Infant Deaths')
-# plt.xlabel('Infant Deaths per 1000 Population')
-# plt.ylabel('Frequency')
-# # plt.show()
-
-
-# df2000 = filtered_data[filtered_data['Year'] == 2000]
-# df2015 = filtered_data[filtered_data['Year'] == 2015]
-
-# # Function to create a bar plot for the top and lowest countries
-# def plot_life_expectancy(ax, df, title):
-#     top_countries_high = df.nlargest(10, 'Life_expectancy')
-#     lowest_countries = df.nsmallest(10, 'Life_expectancy')
-#     combined_df = pd.concat([top_countries_high, lowest_countries])
-
-#     # Plot the data for highest life expectancy
-#     ax.barh(combined_df['Country'][:10], combined_df['Life_expectancy'][:10], color='skyblue', label='Lowest')
-
-#     # Plot the data for lowest life expectancy
-#     ax.barh(combined_df['Country'][10:], combined_df['Life_expectancy'][10:], color='orange', label='Highest')
-
-#     ax.set_title(title)
-#     ax.set_xlabel('Life Expectancy')
-#     ax.set_ylabel('Country')
-#     ax.legend(title='Life Expectancy')
-
-# # Create subplots
-# fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 10))
-
-# # Plot for 2000
-# plot_life_expectancy(axes[0], df2000, 'Top 10 Countries with Highest and Lowest Life Expectancy (Year 2000)')
-
-# # Plot for 2015
-# plot_life_expectancy(axes[1], df2015, 'Top 10 Countries with Highest and Lowest Life Expectancy (Year 2015)')
-
-# plt.tight_layout()
-# # plt.show()
-
-# def groupedBarPlot(filtered_data):
-#     # Filter years from 2000 to 2015 with a skip of 5 years
-#     years_to_plot = np.arange(2000, 2016, 5)
-
-#     # Filter data for selected years
-#     filtered_data_subset = filtered_data[filtered_data['Year'].isin(years_to_plot)]
-
-#     # Create a grouped barplot
-#     plt.figure(figsize=(12, 8))
-#     sns.barplot(x='Year', y='GDP_per_capita', hue='Country', data=filtered_data_subset, palette='Set1')
-
-#     # Adding labels and  title
-#     plt.xlabel('Year')
-#     plt.ylabel('GDP_per_capita')
-#     plt.title('Grouped Barplot of GDP_per_capita Over the Years')
-#     plt.legend(title='Country', bbox_to_anchor=(1.05, 1), loc='upper left')
-
-#     # Display the plot
-#     plt.tight_layout()
-#     # plt.show()
-
-# # Call the groupedBarPlot function with your filtered data
-# groupedBarPlot(filtered_data)
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -114,36 +19,101 @@ filtered_data.dropna(inplace=True)
 # Save filtered data to CSV
 filtered_data.to_csv("filtered.csv", index=False)
 
-# Distribution of infant deaths
-plt.figure(figsize=(8, 6))
-sns.histplot(filtered_data['Infant_deaths'], bins=20, kde=True)
-plt.title('Distribution of Infant Deaths')
-plt.xlabel('Infant Deaths per 1000 Population')
-plt.ylabel('Frequency')
-plt.show()
+# Define the missing function
+def pie_chart_health_metrics(ax, df, metric, title):
+    # Group the data by region and sum the selected metric
+    grouped_df = df.groupby('Region')[metric].sum().reset_index()
 
-def groupedBarPlot(filtered_data):
+    # Plot a pie chart
+    ax.pie(grouped_df[metric], labels=grouped_df['Region'], autopct='%1.1f%%', startangle=140, colors=sns.color_palette('pastel'))
+    ax.set_title(title)
+
+# Define the groupedBarPlot function
+def groupedBarPlot(ax, df):
     # Filter years from 2000 to 2015 with a skip of 5 years
     years_to_plot = np.arange(2000, 2016, 5)
 
     # Filter data for selected years
-    filtered_data_subset = filtered_data[filtered_data['Year'].isin(years_to_plot)]
+    filtered_data_subset = df[df['Year'].isin(years_to_plot)]
 
     # Create a grouped barplot
-    plt.figure(figsize=(12, 8))
-    sns.barplot(x='Year', y='GDP_per_capita', hue='Region', data=filtered_data_subset, palette='Set1',ci=None)
+    sns.barplot(x='Year', y='GDP_per_capita', hue='Region', data=filtered_data_subset, palette='Set1', ci=None, ax=ax)
 
-    # Adding labels and  title
-    plt.xlabel('Year')
-    plt.ylabel('GDP_per_capita')
-    plt.title('Grouped Barplot of GDP_per_capita Over the Years')
-    plt.legend(title='Region', bbox_to_anchor=(1.05, 1), loc='upper left')
+    # Adding labels and title
+    ax.set_xlabel('Year')
+    ax.set_ylabel('GDP_per_capita')
+    ax.set_title('Grouped Barplot of GDP_per_capita Over the Years')
+    ax.legend(title='Region', bbox_to_anchor=(1.05, 1), loc='upper left')
 
-    # Display the plot
-    plt.tight_layout()
-    plt.show()
+# Define the horizontalBarPlot function
+def horizontalBarPlot(ax, df):
+    # Sort data by population in descending order
+    df = df.sort_values(by='Population_mln', ascending=False)
 
-# Call the groupedBarPlot function with your filtered data
-groupedBarPlot(filtered_data)
+    # Define custom colors for each region
+    region_colors = {
+        'Africa': 'blue',
+        'North America': 'green',
+        'South America': 'red',
+        'Asia': 'purple',
+        'Middle East': 'orange'
+    }
 
+    # Create a horizontal bar plot with different colors for each region
+    ax.barh(df['Region'], df['Population_mln'], color=[region_colors[region] for region in df['Region']], alpha=0.7)
 
+    ax.set_xlabel("Population [in Millions]")
+    ax.set_ylabel("Region")
+    ax.set_title("Population Distribution by Region")
+
+# Create a figure with 2 rows and 4 columns
+fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(20, 12))
+
+# First column: Grouped Bar Plot
+groupedBarPlot(axes[0, 0], filtered_data)
+axes[0, 0].set_facecolor(sns.color_palette('pastel')[0])  # Set background color to pastel
+
+# Second column: Pie chart for under-five deaths
+pie_chart_health_metrics(axes[0, 1], filtered_data, 'Under_five_deaths', 'Under-Five Deaths by Region')
+axes[0, 1].set_facecolor(sns.color_palette('pastel')[1])  # Set background color to pastel
+
+# Third column: Pie chart for infant deaths
+pie_chart_health_metrics(axes[0, 2], filtered_data, 'Infant_deaths', 'Infant Deaths by Region')
+axes[0, 2].set_facecolor(sns.color_palette('pastel')[2])  # Set background color to pastel
+
+# Fourth column: Line plot for life expectancy
+sns.lineplot(x='Year', y='Life_expectancy', hue='Region', data=filtered_data, palette='Set1', marker='o', ax=axes[0, 3])
+axes[0, 3].set_facecolor(sns.color_palette('pastel')[3])  # Set background color to pastel
+
+# Fifth column: Horizontal bar plot for population distribution
+horizontalBarPlot(axes[1, 0], filtered_data)
+axes[1, 0].set_facecolor(sns.color_palette('pastel')[4])  # Set background color to pastel
+
+# Sixth column: Description for Grouped Bar Plot
+axes[1, 1].text(0.5, 0.5, "This plot shows the GDP per capita over the years for different regions.", 
+                horizontalalignment='center', verticalalignment='center', fontsize=12)
+axes[1, 1].axis('off')
+axes[1, 1].set_facecolor(sns.color_palette('pastel')[5])  # Set background color to pastel
+
+# Seventh column: Description for Pie Charts
+axes[1, 2].text(0.5, 0.5, "These pie charts represent the distribution of under-five deaths and infant deaths by region.", 
+                horizontalalignment='center', verticalalignment='center', fontsize=12)
+axes[1, 2].axis('off')
+axes[1, 2].set_facecolor(sns.color_palette('pastel')[6])  # Set background color to pastel
+
+# Eighth column: Description for Horizontal Bar Plot
+axes[1, 3].text(0.5, 0.5, "This plot illustrates the population distribution across regions.", 
+                horizontalalignment='center', verticalalignment='center', fontsize=12)
+axes[1, 3].axis('off')
+axes[1, 3].set_facecolor(sns.color_palette('pastel')[7])  # Set background color to pastel
+
+# Add a title to the entire figure
+plt.suptitle("Life Expectancy Over Different Regions (2000-2015) - A Study", fontsize=16, fontweight='bold')
+
+# Save the figure
+plt.tight_layout(pad=4.0, h_pad=6.0, w_pad=2.0)
+plt.subplots_adjust(wspace=0.8, hspace=0.6) 
+plt.savefig('combined_plots_with_description.png', dpi=300)  # Save as PNG file with higher resolution (dpi)
+
+# Show the entire figure
+plt.show()
